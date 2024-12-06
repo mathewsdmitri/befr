43855cbeec6526fa662d0ceed350df6923e993a7
onlyinvalid <61599367+onlyinvalid@users.noreply.github.com>
2024-12-06 14:06:41 -0800

    made changes to the UI
    still having trouble with the svg icon files

--- myproj/lib/homePage.dart
+++ myproj/lib/homePage.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('HomePage'), centerTitle: true),
+      appBar: AppBar(title: const Text(''), centerTitle: true),
       body: songs.isEmpty
           ? const Center(child: Text('No songs available'))
           : ListView.separated(
@@ -21,11 +21,10 @@ class HomePage extends StatelessWidget {
                   title: Text(song['track_name']),
                   subtitle: Text(song['artist_name']),
                 );
-                
               },
               separatorBuilder: (context, index) {
-                   return const Divider(color: Colors.black, thickness: 1);
-                 },
+                return const Divider(color: Colors.black, thickness: 1);
+              },
             ),
     );
   }
